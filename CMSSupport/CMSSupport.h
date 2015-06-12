2941899a2a19cff38fa672d9e3d833f50a6638e3
Erin Mounts <erinys62@gmail.com>
2015-06-12 11:14:10 -0700

    Initial Commit

---
+++ CMSSupport/CMSSupport.h
@@ -0,0 +1,19 @@
+//
+//  CMSSupport.h
+//  CMSSupport
+//
+//  Created by Erin Mounts on 6/12/15.
+//  Copyright (c) 2015 Erin Mounts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMSSupport.
+FOUNDATION_EXPORT double CMSSupportVersionNumber;
+
+//! Project version string for CMSSupport.
+FOUNDATION_EXPORT const unsigned char CMSSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMSSupport/PublicHeader.h>
+
+
