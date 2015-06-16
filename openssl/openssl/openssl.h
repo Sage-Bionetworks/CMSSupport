019a3675aa14aed2381452beb45299b8ae3fd0eb
Erin Mounts <erinys62@gmail.com>
2015-06-16 13:19:27 -0700

    a bit further

---
+++ openssl/openssl/openssl.h
@@ -0,0 +1,19 @@
+//
+//  openssl.h
+//  openssl
+//
+//  Created by Erin Mounts on 6/16/15.
+//  Copyright (c) 2015 Erin Mounts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openssl.
+FOUNDATION_EXPORT double opensslVersionNumber;
+
+//! Project version string for openssl.
+FOUNDATION_EXPORT const unsigned char opensslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openssl/PublicHeader.h>
+
+
