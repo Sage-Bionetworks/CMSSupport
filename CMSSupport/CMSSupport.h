f4f61617f4e36c845a3f7c5870105f3b27f01a4c
Shannon Young <shannon.young@sagebase.org>
2021-04-13 15:24:26 -0700

    Fix header copyright

--- CMSSupport/CMSSupport.h
+++ CMSSupport/CMSSupport.h
@@ -4,7 +4,7 @@
 //
 //  Created by Erin Mounts on 6/12/15.
 //
-//	Copyright (c) 2105, Sage Bionetworks
+//	Copyright (c) 2015-2021, Sage Bionetworks
 //	All rights reserved.
 //
 //	Redistribution and use in source and binary forms, with or without
