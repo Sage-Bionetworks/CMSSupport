7390eb96b9edf53c3127d2aa2641a9475b36136e
Shannon Young <shannon.young@sagebase.org>
2021-04-13 17:24:18 -0700

    Fix copyrights and version tagging

--- Package.swift
+++ Package.swift
@@ -4,18 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenSSL",
+    name: "CMSSupport",
     platforms: [
         .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "openssl",
-            targets: ["openssl"]),
-        .library(
             name: "CMSSupport",
-            targets: ["CMSSupport"]),
+            targets: ["CMSSupport", "openssl"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
